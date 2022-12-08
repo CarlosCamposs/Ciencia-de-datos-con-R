4a5261b51bcadb0ca9fd7a1ebddf50f467b53dee
CarlosCamposs <carlosdanct7@gmail.com>
2022-12-08 15:06:18 -0600

    Clase 04

---
+++ Scripts/Clase04.R
@@ -0,0 +1,15 @@
+# ///////////////////////////////////////////////////////////////////////////
+# Sesion 04 - Ciencia de datos con R
+
+
+library(nycflights13)
+library(tidyverse)
+library(knitr)
+
+##########
+# Cargamos los datos
+setwd("C:/Users/Carlos Daniel/Desktop/GitHub/Ciencia-de-datos-con-R/datasets")
+
+
+
+# ------------------------------------------------------------------------------
