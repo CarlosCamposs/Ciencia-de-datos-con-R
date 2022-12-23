ffd708137c27dd4847c222a3904e8ad46b60c5f8
CarlosCamposs <carlosdanct7@gmail.com>
2022-12-23 15:13:43 -0600

    Clase 15

---
+++ Scripts/Clase15.R
@@ -0,0 +1,32 @@
+# ///////////////////////////////////////////////////////////////////////////
+# Sesion 15 - Ciencia de datos con R
+
+
+library(nycflights13)
+library(tidyverse)
+library(knitr)
+library(moderndive)
+library(scales) 
+library(zoo)
+
+
+##########
+# Cargamos los datos
+setwd("C:/Users/Carlos Daniel/Desktop/GitHub/Ciencia-de-datos-con-R/datasets")
+
+# ------------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
